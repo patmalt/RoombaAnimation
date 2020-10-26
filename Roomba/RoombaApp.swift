fada0e23881406f669031224c01e5c5913e3b9f3
Patrick Maltagliati <patmalt@gmail.com>
2020-10-26 09:39:32 -0500

    Initial Commit

---
+++ Roomba/RoombaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoombaApp.swift
+//  Roomba
+//
+//  Created by Patrick Maltagliati on 10/26/20.
+//
+
+import SwiftUI
+
+@main
+struct RoombaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
