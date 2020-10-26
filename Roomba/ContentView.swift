68d595d9c0de1dd31ea9d88f6121c2ffaecbd940
Patrick Maltagliati <patmalt@gmail.com>
2020-10-26 11:16:10 -0500

    Recreate roomba animation

--- Roomba/ContentView.swift
+++ Roomba/ContentView.swift
@@ -6,11 +6,69 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LineGrid(percent: viewModel.percent)
+                .stroke(lineWidth: 1)
+                .foregroundColor(Color.gray)
+                .padding()
+                .animation(Animation.linear(duration: 2).repeatForever(autoreverses: false))
+                .mask(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.black]), startPoint: .top, endPoint: .center))
+                .mask(LinearGradient(gradient: Gradient(colors: [Color.black, Color.clear]), startPoint: .center, endPoint: .bottom))
+            Circle().fill().frame(width: 100, height: 100).shadow(radius: 1)
+        }
+        .onAppear {
+            viewModel.animate()
+        }
+    }
+}
+
+struct LineGrid: Shape {
+    var percent: CGFloat
+    
+    var animatableData: CGFloat {
+        get { percent }
+        set { percent = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: 0))
+        path.closeSubpath()
+        
+        path.move(to: CGPoint(x: 0, y: percent * rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: percent * rect.maxY))
+        
+        path.move(to: CGPoint(x: 0, y: percent * rect.maxY - (rect.maxY / 3)))
+        path.addLine(to: CGPoint(x: rect.maxX, y: percent * rect.maxY - (rect.maxY / 3)))
+        
+        path.move(to: CGPoint(x: 0, y: percent * rect.maxY - (2 * rect.maxY / 3)))
+        path.addLine(to: CGPoint(x: rect.maxX, y: percent * rect.maxY - (2 * rect.maxY / 3)))
+        
+        path.move(to: CGPoint(x: 0, y: percent * rect.maxY + (rect.maxY / 3)))
+        path.addLine(to: CGPoint(x: rect.maxX, y: percent * rect.maxY + (rect.maxY / 3)))
+        
+        path.move(to: CGPoint(x: 0, y: percent * rect.maxY + (2 * rect.maxY / 3)))
+        path.addLine(to: CGPoint(x: rect.maxX, y: percent * rect.maxY + (2 * rect.maxY / 3)))
+        
+        return path
+    }
+}
+
+class ViewModel: ObservableObject {
+    @Published var percent: CGFloat = 0
+    private var disposeBag = Set<AnyCancellable>()
+    
+    func animate() {
+        percent = 1
     }
 }
 
